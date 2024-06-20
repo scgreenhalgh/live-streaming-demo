2a2bd22cc1eb5a20f7a213760883742134b517dc
scgreenhalgh <me@seangreenhalgh.com>
2024-06-20 12:19:58 +0800

    chore: Update port number to 3001 in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 
-const port = 3000;
+const port = 3001;
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
